7bff245e1c481a827678358587c302a8def64d6e
Eric To <erto@berkeley.edu>
2019-03-19 18:06:24 -0700

    Add todo app features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-import TodoList from './TodoList';
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn more about React", isDone: false },
+    { text: "Meet with Julian for lunch", isDone: false },
+    { text: "Play The Division 2", isDone: false }
+  ]);
 
-class App extends Component {
-  render() {
-    return (
-      <TodoList />
-    );
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
   }
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+// const Todo = ({ todo }) => <div className="todo">{todo.text}</div>
+
+function Todo({ todo, index, completeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isDone ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  )
 }
 
 export default App;
